bfd3097b4491306b82fe33fd3eff60c39b5126f1
ali26m <alihisham26m@gmail.com>
2025-06-01 00:20:20 +0300

    update get real locaion and deploy it. wait for testing

--- web/get_real_location/send_location.py
+++ web/get_real_location/send_location.py
@@ -53,4 +53,4 @@ def track_location():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
